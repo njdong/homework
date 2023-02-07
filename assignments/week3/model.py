60ad25e9edc5890ff8927b09095c9a4e15e601bf
njdong <ningjund@gmail.com>
2023-02-07 03:10:11 -0500

    Update model.py

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,16 +1,17 @@
 import torch
+import torch.nn as nn
 from typing import Callable
 
 
-class MLP:
+class MLP(nn.Module):
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+        activation: Callable = nn.ReLU,
+        initializer: Callable = nn.init.ones_,
     ) -> None:
         """
         Initialize the MLP.
@@ -22,7 +23,21 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.hidden_count = hidden_count
+        self.activation = activation
+
+        self.layers = nn.ModuleList([nn.Linear(input_size, hidden_size)])
+
+        for i in range(hidden_count - 1):
+            self.layers.append(nn.Linear(hidden_size, hidden_size))
+        self.layers.append(nn.Linear(hidden_size, num_classes))
+
+        for layer in self.layers:
+            initializer(layer.weight)
 
     def forward(self, x):
         """
@@ -34,4 +49,9 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        for layer in self.layers[:-1]:
+            x = self.activation()(layer(x))
+
+        x = self.layers[-1](x)
+
+        return x
