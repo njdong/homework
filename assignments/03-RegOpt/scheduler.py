532aa9571355f3ba6cd3bd9f0082b01e812c4b00
njdong <122651889+njdong@users.noreply.github.com>
2023-02-19 19:50:36 -0500

    Update scheduler.py

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -2,9 +2,11 @@ from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
 
+import numpy as np
+
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(self, optimizer, step_size=2, gamma=0.005, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -14,6 +16,13 @@ class CustomLRScheduler(_LRScheduler):
         """
         # ... Your Code Here ...
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        
+        self.optimizer = optimizer
+       
+        self.step_size = step_size
+        self.gamma = gamma
+        self.last_epoch = last_epoch
+        
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
@@ -21,4 +30,14 @@ class CustomLRScheduler(_LRScheduler):
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        """
+        getting learning rate; assuming the learning rate decays exponentially with a facter of gamma
+        """
+
+
+        if (self.last_epoch == 0):
+            return [lr for lr in self.base_lrs]
+
+        decay = np.exp((-1) * self.gamma)
+        
+        return [lr * decay for lr in self.base_lrs]
