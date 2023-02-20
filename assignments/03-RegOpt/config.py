7a3a322796f18587638d4949bd2108803094aef8
njdong <ningjund@gmail.com>
2023-02-19 20:00:14 -0500

    Update config.py

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,23 +6,22 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
     num_epochs = 2
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
-        # You can pass arguments to the learning rate scheduler
-        # constructor here.
-        'step_size': 1,
-        'gamma': 0.005,
+        "step_size": 1,
+        "gamma": 0.005,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
