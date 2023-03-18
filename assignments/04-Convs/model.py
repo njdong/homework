22d112da0de820a5377fa068d56cbca9a6bb228a
njdong <122651889+njdong@users.noreply.github.com>
2023-03-18 12:56:10 -0400

    Add files via upload

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -5,44 +5,31 @@ import torch.nn.functional as F
 
 class Model(torch.nn.Module):
     """
-    CNN with (conv1 -> batchnorm1)*6 -> pooling
+    CNN (LeNet)
     """
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """
-        CNN with (conv1 -> batchnorm1)*6 -> pooling
+        CNN (LeNet)
         """
-        super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(
-            num_channels, 32, kernel_size=3, stride=2, padding=1, bias=False
-        )
+
+        self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.conv5 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn5 = nn.BatchNorm2d(512)
-        self.conv6 = nn.Conv2d(
-            512, 1024, kernel_size=3, stride=2, padding=1, bias=False
-        )
-        self.bn6 = nn.BatchNorm2d(1024)
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(1024, num_classes)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
-        CNN with (conv1 -> batchnorm1)*6 -> pooling
+        CNN (conv1 -> batchnorm1 -> relu -> maxpoll)*2
         """
         x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
         x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = F.relu(self.bn5(self.conv5(x)))
-        x = F.relu(self.bn6(self.conv6(x)))
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 64 * 8 * 8)
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = self.fc2(x)
         return x
